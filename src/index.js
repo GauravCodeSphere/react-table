bf13be138982dc483f75279188774460ef90ea32
Gaurav Talesara <153828287+GauravCodeSphere@users.noreply.github.com>
2024-01-18 10:45:32 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -3,12 +3,21 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { Provider } from 'react-redux';
+import store from './store';
+import { DndProvider } from 'react-dnd';
+import { HTML5Backend } from 'react-dnd-html5-backend';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
+  // <React.StrictMode>
+    <Provider store={store}>
+      <DndProvider backend={HTML5Backend}>
+
     <App />
-  </React.StrictMode>
+      </DndProvider>
+    </Provider>
+  // </React.StrictMode>
 );
 
 // If you want to start measuring performance in your app, pass a function
