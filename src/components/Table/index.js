965bc45201a1de5a7dfaeb99a28acd181930fd4e
Gaurav Talesara <153828287+GauravCodeSphere@users.noreply.github.com>
2024-01-19 19:03:53 +0530

    update table

--- src/components/Table/index.js
+++ src/components/Table/index.js
@@ -1,6 +1,7 @@
 import React, { useState } from 'react';
 import { connect } from 'react-redux';
 import { FaSort, FaSortUp, FaSortDown } from "react-icons/fa";
+import { IoMdArrowDropleftCircle, IoMdArrowDroprightCircle } from "react-icons/io";
 import SearchForm from './SearchForm';
 import TableActions from './TableActions';
 import ProductItem from './ProductItem';
@@ -11,12 +12,13 @@ import { ExportCSVButton } from '../../hooks/ExportCSVButton';
 import { actions, buttonStyles, columnLabels } from '../../utils/material';
 
 // import custom hooks 
-import { useFilteredAndSortedProducts, useSorting, useSearch, useFieldCount, usePagination, useColumnVisibility, useColumnSearch } from '../../hooks';
+import { useFilteredAndSortedProducts, useSorting, useSearch, useFieldCount, usePagination, useColumnVisibility, useColumnSearch, useUndo, useSmoothScrolling } from '../../hooks';
 import { useActions } from '../../store/actions';
 
 const ProductTable = ({ products, loading, error }) => {
 
     const { deleteMultiProduct } = useActions()
+    const { containerRef, startScrolling, stopScrolling } = useSmoothScrolling();
     const { currentPage, itemsPerPage, handlePageChange, handleItemsPerPageChange, startIndex, endIndex } = usePagination(10)
     const { selectedColumns, handleColumnChange, setSelectedColumns } = useColumnVisibility();
     const { searchTerm, handleSearch } = useSearch(handlePageChange);
@@ -51,6 +53,8 @@ const ProductTable = ({ products, loading, error }) => {
         setSelectedItems([])
     };
 
+
+
     // Function to toggle the presence of a brand in the selectedBrand array
     const toggleBrand = (brand) => {
         setSelectedBrand(prevSelectedBrand => {
@@ -72,12 +76,35 @@ const ProductTable = ({ products, loading, error }) => {
         handleColumnSearchChange(column, value);
         handlePageChange(1);
     };
+    const handleReset = (column) => {
+        handleColumnSearchChange(column, ''); // Clear the search term for the specified column
+        handlePageChange(1);
+    };
+
 
     return (
         <section className=" dark:bg-gray-900 p-3 sm:p-5 h-screen ">
             <div className="mx-auto max-w-screen-xl px-4 lg:px-12">
                 <div className="bg-white dark:bg-gray-800 relative shadow-lg sm:rounded-lg overflow-hidden">
                     <div className='flex justify-end mt-3 me-4 gap-3'>
+                        <div className="flex justify-between px-4 mt-2">
+                            <button
+                                className="text-sky-500 text-lg px-4 py-2 border rounded hover:bg-slate-50"
+                                onMouseDown={() => startScrolling(-1)}
+                                onMouseUp={stopScrolling}
+                                onMouseLeave={stopScrolling}
+                            >
+                                <IoMdArrowDropleftCircle />
+                            </button>
+                            <button
+                                className="text-sky-500 text-lg px-4 py-2 border rounded hover:bg-slate-50"
+                                onMouseDown={() => startScrolling(1)}
+                                onMouseUp={stopScrolling}
+                                onMouseLeave={stopScrolling}
+                            >
+                                <IoMdArrowDroprightCircle />
+                            </button>
+                        </div>
                         <JsonView productData={filteredProductsByBrand} />
                         <ExportCSVButton data={filteredProductsByBrand} filename="table_data" />
                     </div>
@@ -94,7 +121,7 @@ const ProductTable = ({ products, loading, error }) => {
                             setSelectedColumns={setSelectedColumns}
                         />
                     </div>
-                    <div className="overflow-x-auto">
+                    <div className="overflow-x-auto" ref={containerRef}>
                         {selectedItems.length > 0 &&
                             <div className='bg-sky-500 text-white ps-3 px-2 font-semibold'>
                                 Selected Items: {selectedItems.length}
@@ -106,14 +133,38 @@ const ProductTable = ({ products, loading, error }) => {
                                     {[...selectedColumns.keys()].map((column, index) => (
                                         <th key={index} scope="col" className="px-4 py-3">
                                             <div className="flex flex-col">
-                                                <input
-                                                    type="text"
-                                                    placeholder={`Search ${getColumnHeaderLabel(column)}`}
-                                                    value={columnSearchTerms[column] || ''}
-                                                    onChange={(event) => handleColumnSearch(event, column)}
-                                                    className="px-2 py-1 border border-gray-300 rounded-md text-sm focus:outline-none focus:ring focus:border-blue-300"
-                                                />
-                                                <div className="flex justify-center items-center mt-1">
+                                                <div className='relative'>
+                                                    <input
+                                                        type="text"
+                                                        placeholder={`Search ${getColumnHeaderLabel(column)}`}
+                                                        value={columnSearchTerms[column] || ''}
+                                                        onChange={(event) => handleColumnSearch(event, column)}
+                                                        className="px-2 py-1 border border-gray-300 rounded-md text-sm focus:outline-none focus:ring focus:border-blue-300"
+                                                    />
+
+                                                    <button
+                                                        type="button"
+                                                        className="absolute inset-y-0 right-0 flex items-center pr-3 focus:outline-none"
+                                                        onClick={() => handleReset(column)}
+                                                    >
+                                                        <svg
+                                                            className="w-4 h-4 text-gray-500 dark:text-gray-400 cursor-pointer"
+                                                            fill="none"
+                                                            stroke="currentColor"
+                                                            viewBox="0 0 24 24"
+                                                            xmlns="http://www.w3.org/2000/svg"
+                                                        >
+                                                            <path
+                                                                strokeLinecap="round"
+                                                                strokeLinejoin="round"
+                                                                strokeWidth="2"
+                                                                d="M6 18L18 6M6 6l12 12"
+                                                            ></path>
+                                                        </svg>
+                                                    </button>
+                                                </div>
+
+                                                <div className="flex justify-start items-center mt-2" onClick={() => handleSort(column)}>
                                                     {getColumnHeaderLabel(column)}
                                                     <span className="ml-1">
                                                         {sortColumn === column ? (
