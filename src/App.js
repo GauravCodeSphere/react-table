77fb2cdc3255c4d7fb2ccf49ff9a12463da85a72
Gaurav Talesara <153828287+GauravCodeSphere@users.noreply.github.com>
2024-01-19 10:47:37 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
 
 import './App.css';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
-import PrivateLayout from './layout/PrivateLayout/PrivateLayout';
+// import PrivateLayout from './layout/PrivateLayout/PrivateLayout';
 import PublicLayout from './layout/PublicLayout/PublicLayout';
+import { Toaster } from 'react-hot-toast';
 
 const App = () => {
 
   return (
     <Router>
+      <Toaster position='top-center' />
       <Routes>
         {/* <Route path="/*" element={<PrivateLayout />} /> */}
         <Route path="/*" element={<PublicLayout />} />
