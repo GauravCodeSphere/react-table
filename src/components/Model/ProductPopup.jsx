86904f7700496d59f8f2aae7c5eb2bbf08728b90
Gaurav Talesara <153828287+GauravCodeSphere@users.noreply.github.com>
2024-01-19 10:48:04 +0530

    Create ProductPopup.jsx

---
+++ src/components/Model/ProductPopup.jsx
@@ -0,0 +1,202 @@
+import { useState } from "react";
+import { useDispatch } from "react-redux";
+import { createProduct, updateProduct } from "../../store/actions/product";
+
+const ProductPopup = ({ isOpen, onClose, productData, name }) => {
+
+  const dispatch = useDispatch()
+  const [editedProduct, setEditedProduct] = useState({
+    title: productData.title || '',
+    category: productData.category || '',
+    brand: productData.brand || '',
+    description: productData.description || '',
+    price: productData.price || '',
+  });
+
+  const handleInputChange = (e) => {
+    const { name, value } = e.target;
+    setEditedProduct((prevProduct) => ({
+      ...prevProduct,
+      [name]: value,
+    }));
+  };
+
+  const handleSave = (e) => {
+    e.preventDefault()
+    console.log('Saving product:', editedProduct);
+
+    if (name === "Create Product") {
+      dispatch(createProduct(editedProduct));
+      onClose();
+    } else {
+      dispatch(updateProduct(productData.id, editedProduct));
+      onClose();
+    }
+
+  };
+
+  return (
+    isOpen && (
+      <div
+        id="default-modal"
+        tabIndex="-1"
+        aria-hidden="true"
+        className="fixed inset-0 overflow-y-auto overflow-x-hidden z-50 flex justify-center items-center bg-[#00000078]"
+      >
+        <div className="relative p-4 w-full max-w-2xl">
+          {/* Modal content */}
+          <div className="relative bg-white rounded-lg shadow dark:bg-gray-700">
+            {/* Modal header */}
+            <div className="flex items-center justify-between p-4 md:p-5 border-b rounded-t dark:border-gray-600">
+              <h3 className="text-xl font-semibold text-gray-900 dark:text-white">
+                {name}
+              </h3>
+              <button
+                onClick={onClose}
+                type="button"
+                className="text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm w-8 h-8 ms-auto inline-flex justify-center items-center dark:hover:bg-gray-600 dark:hover:text-white"
+              >
+                <svg
+                  className="w-3 h-3"
+                  aria-hidden="true"
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="none"
+                  viewBox="0 0 14 14"
+                >
+                  <path
+                    stroke="currentColor"
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    strokeWidth="2"
+                    d="m1 1 6 6m0 0 6 6M7 7l6-6M7 7l-6 6"
+                  />
+                </svg>
+                <span className="sr-only">Close modal</span>
+              </button>
+            </div>
+
+            {/* Modal body */}
+            <div className="p-4 md:p-5 space-y-4">
+              <form onSubmit={handleSave}>
+                <div className="grid gap-6 mb-6 sm:grid-cols-2">
+                  <div>
+                    <label
+                      htmlFor="productName"
+                      className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+                    >
+                      Product Name
+                    </label>
+                    <input
+                      type="text"
+                      defaultValue={editedProduct.title}
+                      id="title"
+                      name="title"
+                      className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                      placeholder="iphone 9"
+                      required
+                      onChange={handleInputChange}
+                    />
+                  </div>
+                  <div>
+                    <label
+                      htmlFor="category"
+                      className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+                    >
+                      Category
+                    </label>
+                    <input
+                      type="text"
+                      id="category"
+                      name="category"
+                      defaultValue={editedProduct.category}
+                      className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                      placeholder="smartphones"
+                      required
+                      onChange={handleInputChange}
+
+                    />
+                  </div>
+                  <div>
+                    <label
+                      htmlFor="brand"
+                      className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+                    >
+                      Brand
+                    </label>
+                    <input
+                      type="text"
+                      id="brand"
+                      name="brand"
+                      defaultValue={editedProduct.brand}
+                      className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                      placeholder="Apple"
+                      required
+                      onChange={handleInputChange}
+
+                    />
+                  </div>
+
+                  <div>
+                    <label
+                      htmlFor="price"
+                      className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+                    >
+                      Price
+                    </label>
+                    <input
+                      type="number"
+                      id="price"
+                      name="price"
+                      defaultValue={editedProduct.price}
+                      className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                      placeholder="125"
+                      required
+                      onChange={handleInputChange}
+
+                    />
+                  </div>
+                </div>
+                <div className="mb-6">
+                  <label
+                    htmlFor="description"
+                    className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+                  >
+                    Description
+                  </label>
+                  <textarea
+                    id="description"
+                    name="description"
+                    defaultValue={editedProduct.description}
+                    className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                    placeholder="description"
+                    required
+                    onChange={handleInputChange}
+
+                  />
+                </div>
+
+                <div className="flex gap-3 justify-end">
+                  <button
+                    type="button"
+                    onClick={onClose}
+                    className="text-white bg-red-500 hover:bg-red-600 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+                  >
+                    Cancel
+                  </button>
+                  <button
+                    type="submit"
+                    className="text-white bg-sky-500 hover:bg-sky-600 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+                  >
+                    {name === "Create Product" ? "Create":"Save"}
+                  </button>
+                </div>
+              </form>
+            </div>
+          </div>
+        </div>
+      </div>
+    )
+  );
+};
+
+export default ProductPopup;
