273b7b8214991f0bb1c6b374b6dc03bae52b747b
Gaurav Talesara <153828287+GauravCodeSphere@users.noreply.github.com>
2024-01-19 12:29:12 +0530

    Update material.js
    added the getColumnValue

--- src/utils/material.js
+++ src/utils/material.js
@@ -64,9 +64,25 @@ let toastMessage = {
 const buttonStyles = "w-full md:w-auto flex items-center justify-center py-2 px-4 text-sm font-medium text-gray-900 focus:outline-none bg-white rounded-lg border border-gray-200 hover:bg-gray-100 hover:text-primary-700 focus:z-10 focus:ring-4 focus:ring-gray-200 dark:focus:ring-gray-700 dark:bg-gray-800 dark:text-gray-400 dark:border-gray-600 dark:hover:text-white dark:hover:bg-gray-700";
 
 
+const getColumnValue = (column) => {
+    switch (column) {
+        case 'productName':
+            return "title";
+        case 'category':
+            return "category";
+        case 'brand':
+            return "brand";
+        case 'description':
+            return "description";
+        case 'price':
+            return "price";
+        default:
+            return '';
+    }
+};
 
 
-export { actions, brands, columnLabels, buttonStyles, toastMessage }
+export { actions, brands, columnLabels, buttonStyles, toastMessage,getColumnValue }
 
 
 
