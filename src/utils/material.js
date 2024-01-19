9026d3862d481896b80bc699ed7148813b975f46
Gaurav Talesara <153828287+GauravCodeSphere@users.noreply.github.com>
2024-01-19 10:49:55 +0530

    added toastMessage obj

--- src/utils/material.js
+++ src/utils/material.js
@@ -1,7 +1,9 @@
+
+
 const actions = [
     {
-        id: 'massEdit',
-        label: 'Mass Edit',
+        id: 'deleteSelected',
+        label: 'Delete selected',
         className: 'your-button-styles',
         icon: (
             <svg
@@ -14,6 +16,21 @@ const actions = [
             </svg>
         ),
     },
+    // {
+    //     id: 'exportCSV',
+    //     label: 'Export CSV',
+    //     className: 'your-button-styles',
+    //     icon: (
+    //         <svg
+    //             className="-ml-1 mr-1.5 w-5 h-5"
+    //             fill="currentColor"
+    //             viewBox="0 0 20 20"
+    //             xmlns="http://www.w3.org/2000/svg"
+    //             aria-hidden="true"
+    //         >
+    //         </svg>
+    //     ),
+    // },
 ];
 
 const brands = [
@@ -33,12 +50,23 @@ const columnLabels = {
 };
 
 
+
+
+let toastMessage = {
+    "CREATE_PRODUCT": "Product successfully created.",
+    "DELETE_MULTI_PRODUCT": "Selected products successfully deleted.",
+    "DELETE_PRODUCT": "Product successfully deleted.",
+    "FETCH_PRODUCTS": "Products successfully retrieved.",
+    "UPDATE_PRODUCT": "Product information successfully updated."
+};
+
+
 const buttonStyles = "w-full md:w-auto flex items-center justify-center py-2 px-4 text-sm font-medium text-gray-900 focus:outline-none bg-white rounded-lg border border-gray-200 hover:bg-gray-100 hover:text-primary-700 focus:z-10 focus:ring-4 focus:ring-gray-200 dark:focus:ring-gray-700 dark:bg-gray-800 dark:text-gray-400 dark:border-gray-600 dark:hover:text-white dark:hover:bg-gray-700";
 
 
 
 
-export {actions,brands,columnLabels,buttonStyles }
+export { actions, brands, columnLabels, buttonStyles, toastMessage }
 
 
 
