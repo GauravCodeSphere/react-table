0962e78ddbb82e348de9506169a3a3e0791e9daa
Gaurav Talesara <153828287+GauravCodeSphere@users.noreply.github.com>
2024-01-19 12:30:07 +0530

    Optimized Main Table

--- src/components/Table/index.js
+++ src/components/Table/index.js
@@ -1,38 +1,37 @@
 import React, { useState } from 'react';
+import { connect, useDispatch } from 'react-redux';
+import { FaSort, FaSortUp, FaSortDown } from "react-icons/fa";
 import SearchForm from './SearchForm';
 import TableActions from './TableActions';
 import ProductItem from './ProductItem';
-import { actions, buttonStyles, columnLabels } from '../../utils/material';
-import { connect, useDispatch } from 'react-redux';
 import LoadingTable from './LoadingTable';
 import PaginationModel from './Pagination';
-import { FaSort, FaSortUp, FaSortDown } from "react-icons/fa";
-import { deleteMultiProduct } from '../../store/actions/product';
-import { ExportCSVButton } from '../../hooks';
 import JsonView from '../Model/JsonView';
+import { ExportCSVButton} from '../../hooks/ExportCSVButton';
+import useSorting from '../../hooks/useSorting';
+import useSearch from '../../hooks/useSearch';
+import { deleteMultiProduct } from '../../store/actions/product';
+import { actions, buttonStyles, columnLabels } from '../../utils/material';
+import useFilteredAndSortedProducts from '../../hooks/useFilteredAndSortedProducts';
+import { useFieldCount } from '../../hooks/useFieldCount';
+import { usePagination } from '../../hooks/usePagination';
+import { useColumnVisibility } from '../../hooks/useColumnVisibility';
 
 const ProductTable = ({ products, loading, error }) => {
 
     const dispatch = useDispatch();
+    const { currentPage, itemsPerPage, handlePageChange, handleItemsPerPageChange, startIndex, endIndex } = usePagination(10)
+    const { selectedColumns, handleColumnChange, setSelectedColumns } = useColumnVisibility();
+    const { searchTerm, handleSearch } = useSearch(handlePageChange);
+    const { sortColumn, sortOrder, handleSort, resetSorting } = useSorting();
 
-
-    const initialColumnVisibility = new Map([
-        ['productName', true],
-        ['category', true],
-        ['brand', true],
-        ['description', true],
-        ['price', true]
-    ]);
-    const [currentPage, setCurrentPage] = useState(1);
-    const [itemsPerPage, setItemsPerPage] = useState(10); // Default items per page
-    const [selectedColumns, setSelectedColumns] = useState(initialColumnVisibility);
-    const [searchTerm, setSearchTerm] = useState('');
-    const [sortColumn, setSortColumn] = useState(null);
-    const [sortOrder, setSortOrder] = useState('asc');
     const [selectedBrand, setSelectedBrand] = useState([]);
-
     const [selectedItems, setSelectedItems] = useState([]);
     const [expandedRowIndex, setExpandedRowIndex] = useState(null);
+
+    const { filteredProductsByBrand } = useFilteredAndSortedProducts(products, searchTerm, sortColumn, sortOrder, selectedBrand, 'brand');
+    const filterOptions = useFieldCount(products, 'brand', 1);
+
 
     const toggleExpanded = (index) => {
         setExpandedRowIndex((prevIndex) => (prevIndex === index ? null : index));
@@ -57,10 +56,7 @@ const ProductTable = ({ products, loading, error }) => {
     // Function to toggle the presence of a brand in the selectedBrand array
     const toggleBrand = (brand) => {
         setSelectedBrand(prevSelectedBrand => {
-            // Check if the brand is already in the array
             const brandIndex = prevSelectedBrand.indexOf(brand);
-
-            // If the brand is not in the array, add it; otherwise, remove it
             if (brandIndex === -1) {
                 return [...prevSelectedBrand, brand];
             } else {
@@ -69,132 +65,13 @@ const ProductTable = ({ products, loading, error }) => {
         });
     };
 
-    const handleSort = (column) => {
-        if (column === sortColumn) {
-            // Toggle sort order if clicking on the same column
-            setSortOrder(sortOrder === 'asc' ? 'desc' : 'asc');
-        } else {
-            // Set new sort column and default to ascending order
-            setSortColumn(column);
-            setSortOrder('asc');
-        }
-    };
-    const handlePageChange = (page) => {
-        setCurrentPage(page);
-    };
-
-    const handleItemsPerPageChange = (value) => {
-        setItemsPerPage(value);
-        setCurrentPage(1); // Reset to the first page when changing items per page
-    };
-
-    const startIndex = (currentPage - 1) * itemsPerPage;
-    const endIndex = startIndex + itemsPerPage;
-    const currentProducts = products.slice(startIndex, endIndex);
-
-    const filteredProducts = products.filter(product =>
-        Object.values(product).some(value =>
-            String(value).toLowerCase().includes(searchTerm.toLowerCase())
-        )
-    );
-
-    const getColumnValue = (column) => {
-        switch (column) {
-            case 'productName':
-                return "title";
-            case 'category':
-                return "category";
-            case 'brand':
-                return "brand";
-            case 'description':
-                return "description";
-            case 'price':
-                return "price";
-            default:
-                return '';
-        }
-    };
-
-
-
-
-    // Sorting logic
-    const sortedProducts = [...filteredProducts].sort((a, b) => {
-        const columnValueA = a[getColumnValue(sortColumn)];
-        const columnValueB = b[getColumnValue(sortColumn)];
-
-        let valueA, valueB;
-
-        if (getColumnValue(sortColumn) === 'price') {
-            valueA = columnValueA || 0;
-            valueB = columnValueB || 0;
-        } else {
-            valueA = columnValueA || '';
-            valueB = columnValueB || '';
-        }
-
-        if (sortOrder === 'asc') {
-            return valueA < valueB ? -1 : valueA > valueB ? 1 : 0;
-        } else {
-            return valueA > valueB ? -1 : valueA < valueB ? 1 : 0;
-        }
-    });
-
-    const filteredProductsByBrand = selectedBrand && selectedBrand.length
-        ? sortedProducts.filter(product => selectedBrand.includes(product.brand))
-        : sortedProducts;
-
-
-    // console.log(filteredProductsByBrand);
-    // Function to reset sorting
-    const resetSorting = () => {
-        setSortColumn(null);
-        setSortOrder('asc');
-    };
-
-
-    const handleColumnChange = (selectedColumn) => {
-        setSelectedColumns((prevVisibility) => {
-            const newVisibility = new Map(prevVisibility);
-            newVisibility.set(selectedColumn, !prevVisibility.get(selectedColumn));
-            return newVisibility;
-        });
-    };
-
     const getColumnHeaderLabel = (column) => {
         return columnLabels[column] || column;
     };
 
-
-
-    const handleSearch = (term) => {
-        setSearchTerm(term);
-        setCurrentPage(1); // Reset to the first page when performing a new search
-    };
-
-
-
-    // Create a map to count occurrences of each brand
-    const brandCountMap = new Map();
-    products.forEach(product => {
-        const brand = product.brand;
-        brandCountMap.set(brand, (brandCountMap.get(brand) || 0) + 1);
-    });
-
-    // Extract unique brands with counts
-    const uniqueBrands = [...new Set(products.map(product => product.brand))]
-        .map(brand => ({
-            id: brand.toLowerCase(),
-            label: brand,
-            count: brandCountMap.get(brand)
-        }))
-        .filter(brand => brand.count > 1);
-
-
     return (
         <section className=" dark:bg-gray-900 p-3 sm:p-5 h-screen ">
             <div className="mx-auto max-w-screen-xl px-4 lg:px-12">
-                {/* Start coding here */}
                 <div className="bg-white dark:bg-gray-800 relative shadow-lg sm:rounded-lg overflow-hidden">
                     <div className='flex justify-end mt-3 me-4 gap-3'>
                         <JsonView productData={filteredProductsByBrand} />
@@ -205,14 +82,13 @@ const ProductTable = ({ products, loading, error }) => {
                         {sortColumn === null || <button className={buttonStyles} onClick={resetSorting}>Clear sort</button>}
                         <TableActions
                             actions={actions}
-                            brands={uniqueBrands}
+                            filterOptions={filterOptions}
                             setSelectedBrand={toggleBrand}
                             selectedColumns={selectedColumns}
                             onColumnChange={handleColumnChange}
                             handleDelete={handleDelete}
-
+                            setSelectedColumns={setSelectedColumns}
                         />
-
                     </div>
                     <div className="overflow-x-auto">
                         {selectedItems.length > 0 &&
@@ -236,7 +112,6 @@ const ProductTable = ({ products, loading, error }) => {
                                                             : <FaSort />
                                                         }
                                                     </span>
-
                                                 </div>
                                             </th>
                                         ))}
@@ -273,7 +148,6 @@ const ProductTable = ({ products, loading, error }) => {
                 </div>
             </div>
         </section>
-
     );
 }
 
