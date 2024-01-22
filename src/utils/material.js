bdc1b1b75fb446ef84c8f107925633691e790698
Gaurav Talesara <153828287+GauravCodeSphere@users.noreply.github.com>
2024-01-22 14:32:34 +0530

    Update material.js

--- src/utils/material.js
+++ src/utils/material.js
@@ -35,7 +35,7 @@ let toastMessage = {
 };
 
 
-const buttonStyles = "w-full md:w-auto flex items-center justify-center py-2 px-4 text-sm font-medium text-sky-500 focus:outline-none bg-white rounded-lg border border-sky-500 hover:bg-gray-100 hover:text-primary-700 focus:z-10 focus:ring-4 focus:ring-gray-200 dark:focus:ring-gray-700 dark:bg-gray-800 dark:text-gray-400 dark:border-gray-600 dark:hover:text-white dark:hover:bg-gray-700";
+const buttonStyles = "w-full md:w-auto flex items-center justify-center py-2 px-4 text-sm font-medium text-sky-500 focus:outline-none bg-white rounded-lg border border-sky-500 hover:bg-gray-100 hover:text-primary-700 focus:z-10 focus:ring-4 focus:ring-sky-100 dark:focus:ring-gray-700 dark:bg-gray-800 dark:text-gray-400 dark:border-gray-600 dark:hover:text-white dark:hover:bg-gray-700";
 
 
 const getColumnValue = (column) => {
