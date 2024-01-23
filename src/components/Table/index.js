50cf896e6098382ed9413389622764eca6d29d61
Gaurav Talesara <153828287+GauravCodeSphere@users.noreply.github.com>
2024-01-23 19:39:27 +0530

    Update table

--- src/components/Table/index.js
+++ src/components/Table/index.js
@@ -1,68 +1,44 @@
-import React, { useEffect, useState } from 'react';
+import React, { useState } from 'react';
 import { connect } from 'react-redux';
-import { MdFilterList, MdFilterListOff } from "react-icons/md";
-import { FaSort, FaSortUp, FaSortDown } from "react-icons/fa";
+import ColumnResize from "react-table-column-resizer";
 import { BiSortAlt2 } from "react-icons/bi";
 import { FaArrowUpLong, FaArrowDownLong } from "react-icons/fa6";
-import { IoMdArrowDropleftCircle, IoMdArrowDroprightCircle } from "react-icons/io";
 import SearchForm from './SearchForm';
 import TableActions from './TableActions';
 import ProductItem from './ProductItem';
 import LoadingTable from './LoadingTable';
 import PaginationModel from './Pagination';
-import JsonView from '../Model/JsonView';
-import { ExportCSVButton } from '../../hooks/ExportCSVButton';
-import { actions, buttonStyles, columnLabels } from '../../utils/material';
+import { actions, columnLabels } from '../../utils/material';
 // import custom hooks 
-import { useFilteredAndSortedProducts, useSorting, useSearch, useFieldCount, usePagination, useColumnVisibility, useColumnSearch, useUndo, useSmoothScrolling, useColorManagement } from '../../hooks';
-import { useActions } from '../../store/actions';
-import ColorChange from './ColorChange';
-import { Tooltip } from 'flowbite-react';
-import ColumnResize from "react-table-column-resizer";
+import { useFilteredAndSortedProducts, useSorting, useSearch, useFieldCount, usePagination, useColumnVisibility, useColumnSearch, useSmoothScrolling, useColorManagement, useSelectItems } from '../../hooks';
+import ColumnDropDown from './ColumnDropDown';
+import TableFeatures from './TableFeatures';
 
 
 const ProductTable = ({ products, loading, error }) => {
 
-
-    const { deleteMultiProduct } = useActions()
+    // custom hooks
     const { rowColors, addColor, removeColor } = useColorManagement()
     const { containerRef, startScrolling, stopScrolling } = useSmoothScrolling();
     const { currentPage, itemsPerPage, handlePageChange, handleItemsPerPageChange, startIndex, endIndex } = usePagination(10)
-    const { selectedColumns, handleColumnChange, setSelectedColumns } = useColumnVisibility();
+    const { selectedColumns, handleColumnChange, setSelectedColumns, showAllColumn, visibleColumnsArray } = useColumnVisibility();
     const { searchTerm, handleSearch } = useSearch(handlePageChange);
-    const { sortColumn, sortOrder, handleSort, resetSorting } = useSorting();
+    const { sortColumn, sortOrder, handleSort, resetSorting, handleSortby, disabledSortOption } = useSorting();
 
+    // useState
     const [selectedBrand, setSelectedBrand] = useState([]);
     const [showFilter, setShowFilter] = useState(false);
-    const [selectedItems, setSelectedItems] = useState([]);
     const [expandedRowIndex, setExpandedRowIndex] = useState(null);
 
-    const { filteredProductsByBrand } = useFilteredAndSortedProducts(products, searchTerm, sortColumn, sortOrder, selectedBrand, 'brand');
-    const filterOptions = useFieldCount(products, 'brand', 1);
-
+    // custom hooks
+    const { filteredProductsByBrand } = useFilteredAndSortedProducts(products, searchTerm, sortColumn, sortOrder, selectedBrand, 'category');
+    const filterOptions = useFieldCount(products, 'category', 1);
     const { columnSearchTerms, handleColumnSearchChange, getFilteredProductsByColumn } = useColumnSearch(filteredProductsByBrand);
+    const { selectedItems, selectAllChecked, setSelectedItems, handleCheckboxChange, handleDelete, handleSelectAllChange } = useSelectItems(getFilteredProductsByColumn)
 
     const toggleExpanded = (index) => {
         setExpandedRowIndex((prevIndex) => (prevIndex === index ? null : index));
     };
-
-    const handleCheckboxChange = (itemId) => {
-        const isSelected = selectedItems.includes(itemId);
-
-        if (isSelected) {
-            setSelectedItems((prevSelected) => prevSelected.filter((id) => id !== itemId));
-        } else {
-            setSelectedItems((prevSelected) => [...prevSelected, itemId]);
-        }
-    };
-
-    const handleDelete = () => {
-        if (selectedItems.length === 0) return alert("No selected item found")
-        deleteMultiProduct(selectedItems);
-        setSelectedItems([])
-    };
-
-
 
     // Function to toggle the presence of a brand in the selectedBrand array
     const toggleBrand = (brand) => {
@@ -85,12 +61,11 @@ const ProductTable = ({ products, loading, error }) => {
         handleColumnSearchChange(column, value);
         handlePageChange(1);
     };
+
     const handleReset = (column) => {
         handleColumnSearchChange(column, ''); // Clear the search term for the specified column
         handlePageChange(1);
     };
-
-
 
 
     return (
@@ -98,62 +73,20 @@ const ProductTable = ({ products, loading, error }) => {
             <div className="mx-auto max-w-screen-xl px-4 lg:px-12">
                 <div className="bg-white dark:bg-gray-800 relative shadow-lg sm:rounded-lg overflow-hidden">
 
-                    <div className="flex flex-wrap justify-between mt-3 gap-3 p-4">
-                        <div >
-
-                            <div className="flex items-center space-x-3 mb-2 sm:mb-0">
-                                <button
-                                    className="text-sky-500 text-lg px-4 py-2 border rounded hover:bg-slate-50"
-                                    onMouseDown={() => startScrolling(-1)}
-                                    onMouseUp={stopScrolling}
-                                    onMouseLeave={stopScrolling}
-                                >
-                                    <IoMdArrowDropleftCircle />
-                                </button>
-                                <button
-                                    className="text-sky-500 text-lg px-4 py-2 border rounded hover:bg-slate-50"
-                                    onMouseDown={() => startScrolling(1)}
-                                    onMouseUp={stopScrolling}
-                                    onMouseLeave={stopScrolling}
-                                >
-                                    <IoMdArrowDroprightCircle />
-                                </button>
-                            </div>
-                        </div>
-                        <div className='flex flex-wrap gap-2'>
-
-                            <div className="flex items-center space-x-3 mb-2 sm:mb-0">
-                                <JsonView productData={filteredProductsByBrand} />
-                            </div>
-
-                            <div className="flex items-center space-x-3 mb-2 sm:mb-0">
-                                <ExportCSVButton data={filteredProductsByBrand} filename="table_data" />
-                            </div>
-
-                            <div className="flex items-center space-x-3 mb-2 sm:mb-0">
-                                <ColorChange selectedItems={selectedItems} setSelectedItems={setSelectedItems} addColor={addColor} removeColor={removeColor} />
-                            </div>
-                            
-                            <Tooltip content="Show/Hide filters" className='bg-slate-700 text-xs' arrow={false}>
-                                <div className="flex items-center space-x-3 mb-2 sm:mb-0">
-                                    <button className={buttonStyles} onClick={() => setShowFilter(!showFilter)}>{showFilter ? <MdFilterListOff /> : <MdFilterList />}</button>
-                                </div>
-                            </Tooltip>
-                        </div>
-
-                    </div>
+                    <TableFeatures
+                        filteredProductsByBrand={filteredProductsByBrand}
+                        selectedItems={selectedItems}
+                        setShowFilter={setShowFilter}
+                        setSelectedItems={setSelectedItems}
+                        showFilter={showFilter}
+                        addColor={addColor}
+                        removeColor={removeColor}
+                        startScrolling={startScrolling}
+                        stopScrolling={stopScrolling}
+                    />
 
                     <div className="flex flex-col md:flex-row items-center justify-between space-y-3 md:space-y-0 md:space-x-4 p-4">
                         <SearchForm onSearch={handleSearch} />
-
-                        {sortColumn === null || (
-                            <div className="mb-2 md:mb-0">
-                                <button className={buttonStyles} onClick={resetSorting}>
-                                    Clear sort
-                                </button>
-                            </div>
-                        )}
-
                         <div className="mb-2 md:mb-0">
                             <TableActions
                                 actions={actions}
@@ -171,15 +104,22 @@ const ProductTable = ({ products, loading, error }) => {
                         {selectedItems.length > 0 &&
                             <div className='bg-sky-500 text-white ps-3 px-2 font-semibold'>
                                 Selected Items: {selectedItems.length}
-                            </div>}
+                            </div>
+                        }
                         <table className="w-full text-sm text-left text-gray-500 dark:text-gray-400">
                             <thead className="text-xs text-gray-700 uppercase bg-gray-100 dark:bg-gray-700 dark:text-gray-400">
                                 <tr>
-                                    <th>
+                                    <th className='px-4 py-3 ms-3 flex justify-center mt-2'>
+                                        <input
+                                            type="checkbox"
+                                            checked={selectAllChecked}
+                                            onChange={() => handleSelectAllChange()}
+                                            className="w-4 h-4 bg-gray-100 border-gray-300 rounded text-primary-600 focus:ring-primary-500 dark:focus:ring-primary-600 dark:ring-offset-gray-700 focus:ring-2 dark:bg-gray-600 dark:border-gray-500"
+                                        />
                                     </th>
-                                    {[...selectedColumns.keys()].map((column, index) => (
-                                        <>
 
+                                    {visibleColumnsArray.map((column, index) => (
+                                        <>
                                             <th key={index} scope="col" className={`px-4 py-3 ${sortColumn === column ? 'sorted-column' : ''}`}>
                                                 <div className="flex flex-col">
                                                     {showFilter &&
@@ -214,9 +154,9 @@ const ProductTable = ({ products, loading, error }) => {
                                                             </button>
                                                         </div>}
 
-                                                    <div className="flex justify-start items-center mt-2" onClick={() => handleSort(column)}>
+                                                    <div className="flex justify-start items-center mt-2" >
                                                         {getColumnHeaderLabel(column)}
-                                                        <span className="ml-1">
+                                                        <span className="ml-1" onClick={() => handleSort(column)}>
                                                             {sortColumn === column ? (
                                                                 sortOrder === 'asc' ? (
                                                                     <FaArrowUpLong />
@@ -226,6 +166,21 @@ const ProductTable = ({ products, loading, error }) => {
                                                             ) : (
                                                                 <BiSortAlt2 />
                                                             )}
+                                                        </span>
+                                                        <span className='ml-1'>
+                                                            <ColumnDropDown
+                                                                column={column}
+                                                                resetSorting={resetSorting}
+                                                                handleSortby={handleSortby}
+                                                                clearFilter={() => handleReset(column)}
+                                                                showFilter={() => setShowFilter(true)}
+                                                                handleColumnChange={() => handleColumnChange(column)}
+                                                                showAllColumn={showAllColumn}
+                                                                clearSortState={sortColumn === null}
+                                                                disabledSortOption={disabledSortOption}
+                                                                filterState={showFilter}
+                                                                hiddenColumn={visibleColumnsArray.length === 5}
+                                                            />
                                                         </span>
                                                     </div>
                                                 </div>
@@ -242,6 +197,8 @@ const ProductTable = ({ products, loading, error }) => {
                                     </th>
                                 </tr>
                             </thead>
+
+
                             {loading ?
                                 <LoadingTable numRows={10} numCols={5} />
                                 :
