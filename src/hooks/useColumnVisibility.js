90fdbe890249c246b5d1b2dac1bbe547be2d3ebb
Gaurav Talesara <153828287+GauravCodeSphere@users.noreply.github.com>
2024-01-23 19:38:26 +0530

    Update useColumnVisibility.js

--- src/hooks/useColumnVisibility.js
+++ src/hooks/useColumnVisibility.js
@@ -2,28 +2,37 @@ import { useState } from "react";
 
 
 const initialColumnVisibility = new Map([
-    ['productName', true],
-    ['category', true],
-    ['brand', true],
-    ['description', true],
-    ['price', true]
-  ]);
-  
-  
-  export const useColumnVisibility = () => {
-    const [selectedColumns, setSelectedColumns] = useState(initialColumnVisibility);
-  
-    const handleColumnChange = (selectedColumn) => {
-      setSelectedColumns((prevVisibility) => {
-        const newVisibility = new Map(prevVisibility);
-        newVisibility.set(selectedColumn, !prevVisibility.get(selectedColumn));
-        return newVisibility;
-      });
-    };
-  
-    return {
-      selectedColumns,
-      handleColumnChange,
-      setSelectedColumns
-    };
+  ['productName', true],
+  ['category', true],
+  ['brand', true],
+  ['description', true],
+  ['price', true]
+]);
+
+
+export const useColumnVisibility = () => {
+  const [selectedColumns, setSelectedColumns] = useState(initialColumnVisibility);
+  const visibleColumnsArray = Array.from(selectedColumns)
+    .filter(([key, value]) => value === true)
+    .map(([key]) => key);
+
+  const handleColumnChange = (selectedColumn) => {
+    setSelectedColumns((prevVisibility) => {
+      const newVisibility = new Map(prevVisibility);
+      newVisibility.set(selectedColumn, !prevVisibility.get(selectedColumn));
+      return newVisibility;
+    });
   };
+
+  const showAllColumn = () =>{
+    setSelectedColumns(initialColumnVisibility)
+  }
+
+  return {
+    selectedColumns,
+    handleColumnChange,
+    setSelectedColumns,
+    visibleColumnsArray,
+    showAllColumn
+  };
+};
